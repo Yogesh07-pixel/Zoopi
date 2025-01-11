78a87b672c62b27285afd6f48d9eab12ddad7fff
Yogesh <yogeshrawat518@gmail.com>
2025-01-11 18:39:37 +0530

    Updated the file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import fastify from "fastify";
 import { authRoutes } from "./auth.js";
+import { categoryRoutes, productRoutes } from "./product.js";
 
 
 const prefix = "/api";
 
 export const registerRoutes = async(fastify) => {
     fastify.register(authRoutes, {prefix : prefix});
+    fastify.register(productRoutes, {prefix : prefix});
+    fastify.register(categoryRoutes, {prefix : prefix});
 };
