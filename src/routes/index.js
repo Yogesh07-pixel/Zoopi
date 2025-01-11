4df083c5878e6167847b7e88b0d4a3904f37f826
Yogesh <yogeshrawat518@gmail.com>
2025-01-11 18:01:52 +0530

    Registered the routes and successfully tested with Postman

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import fastify from "fastify";
+import { authRoutes } from "./auth.js";
+
+
+const prefix = "/api";
+
+export const registerRoutes = async(fastify) => {
+    fastify.register(authRoutes, {prefix : prefix});
+};
