9262fa84d64f8031c4675365949a4a713090d798
Yogesh <yogeshrawat518@gmail.com>
2025-01-11 14:43:13 +0530

    Modified Some changes

--- seedScript.js
+++ seedScript.js
@@ -1,24 +1,28 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
-import { Category , Product } from './src/models/index.js'
+import { Category, Product } from './src/models/index.js'
 import { categories, products } from './seedData.js';
 
 
-async function seedDatabase () {
+async function seedDatabase() {
     try {
 
-        
-        // await Product.deleteMany({});
-        // await Category.deleteMany({});
-        
+        // First connect to the DataBase.
+        await mongoose.connect(process.env.MONGO_URI);
+
+        // Deleting all the pre-existind Data.
+        await Product.deleteMany({});
+        await Category.deleteMany({});
+
+        // // Seeding the Category Data into the Category Section.
         const categoryDocs = await Category.insertMany(categories);
-        
+
         const categoryMap = categoryDocs.reduce((map, category) => {
             map[category.name] = category._id;
             return map;
-        },{});
+        }, {});
 
-
+        // Similarly Seeding the Product Data into the Product Section with category assigning.
         const productWithCategoryIds = products.map((product) => ({
             ...product,
             category: categoryMap[product.category],
